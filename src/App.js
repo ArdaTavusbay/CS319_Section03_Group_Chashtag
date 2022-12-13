27d45b65b6349bd1df3a816858db40963d3d09e3
ArdaTavusbay <arda.tavusbay@ug.bilkent.edu.tr>
2022-12-14 01:14:26 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/loginpage/login_page";
+import StudentPage from "./pages/studentpage/student_page";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Routes>
+                    <Route exact path="/loginPage" element={<LoginPage />} />
+                    <Route path="/studentActions" element={<StudentPage />} />
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
